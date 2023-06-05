73c2bf556abae5db3d38eb8251464cd8ab4a9d3c
fathanh123 <91273784+fathanh123@users.noreply.github.com>
2023-06-05 18:58:51 +0700

    finish day 1
    kelar juga api, lanjut front end nanti

--- controller/transactions.js
+++ controller/transactions.js
@@ -1,7 +1,37 @@
 const db = require("../config/connection")
+const transactions = require("../routers/transactions")
 
-exports.checkout = async (data) => {
-    const query =  await db.query('insert into transactions set?', [data])
+exports.checkout = async (order, products) => {
+    const query =  await db.query('insert into transactions set?', [order])
     .catch(err => {return err})
-    return query
+    if(!query.code){
+        let dataProducts = []
+        let idProducts = []
+        products.map( item => {
+            dataProducts.push([
+                order.no_order,
+                item.id, //ini yang dimaksud comment dibawah
+                item.quantity //ini juga
+            ])
+            idProducts.push([
+                item.id
+            ])
+        })
+
+        await db.query('insert into transaction_detail(no_order, id_product, quantity) values ?', [dataProducts])
+
+        const stockProduct = await db.query('select stock from products where id in (?)',[idProducts])
+
+        let updateStock = []
+        stockProduct.map((res, i) => {
+            updateStock.push([
+                dataProducts[i][1], //jadi multiple array ini, 1 mewakili indeks array dari dataProducts
+                res.stock - dataProducts[i][2] // 2 sama seperti yg diatas, indeks array ke-2 dari dataProducts
+            ])
+        })
+
+        await db.query('insert into products (id, stock) values ? on duplicate key update stock = values(stock)', [updateStock])
+    }
+    const dataOrder = await db.query(`select * from transactions where no_order = '${order.no_order}'`)
+    return dataOrder
 }
--- routers/transactions.js
+++ routers/transactions.js
@@ -2,13 +2,14 @@ const express = require('express')
 const transactions = express.Router()
 const { randomOrderNumber } = require("../helpers/utils")
 const { checkout } = require('../controller/transactions')
+const products = require('./products')
 
 transactions.route('/').post(async (req, res) => {
-    const { total_price, paid_amount } = req.body
-    const data = {
+    const { total_price, paid_amount, products} = req.body
+    const order = {
         no_order : randomOrderNumber(), total_price, paid_amount
     }
-    res.send(await checkout(data))
+    res.send(await checkout(order, products))
 })
 
 module.exports = transactions;
